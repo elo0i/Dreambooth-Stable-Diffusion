17122ff32c87572d9e641f9aced3c9b12b786d37
elo0i <114620017+elo0i@users.noreply.github.com>
2023-07-25 20:30:09 +0200

    Update setup_training.py

--- JupyterNotebookHelpers/setup_training.py
+++ JupyterNotebookHelpers/setup_training.py
@@ -13,8 +13,8 @@ class SetupTraining:
     def __init__(self, args):
         self.training_images_save_path = "./training_images"
         self.config_save_path = "./joepenna-dreambooth-configs"
-        self.args = args
         self.selected_model = SDModelOption(repo_id='panopstor/EveryDream', filename='sd_v1-5_vae.ckpt')
+        self.args = args
 
     def submit_form_click(self, b):
         # training images
